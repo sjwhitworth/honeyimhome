2ebc54b658da5aa08c4d13d23676772a663c73cc
Stephen Whitworth <stephen.whitworth@hailocab.com>
2013-12-12 19:58:58 +0000

    Structuring

---
+++ config.py
@@ -0,0 +1,3 @@
+PUSHOVER_URL = "https://api.pushover.net/1/messages.json"
+TOKEN = "aYVgxDfdzRBee6CS693zCtw53vxrGK"
+USER = "O78eqMtpcw891V02TF88eeEwt4o2lq"
---
+++ run.py
@@ -0,0 +1,6 @@
+from config import *
+import requests
+
+# payload = {"user": USER, "token": TOKEN, "message": "Hi, I Love You", "title":"I'm home"}
+
+# j = requests.post(PUSHOVER_URL, data=payload)
